c8651da322b31bf52410e19bf77372d8257821c8
kitsuya0828 <kitsuyaazuma@gmail.com>
2024-12-08 22:33:53 +0900

    feat: introduce BaseModel for enhanced type hinting

--- src/blazefl/core/__init__.py
+++ src/blazefl/core/__init__.py
@@ -2,7 +2,7 @@ from blazefl.core.client_trainer import SerialClientTrainer, ParallelClientTrain
 from blazefl.core.model_selector import ModelSelector
 from blazefl.core.partitioned_dataset import PartitionedDataset
 from blazefl.core.server_handler import ServerHandler
-from blazefl.core.shared_data import SharedData, ShareMethod
+from blazefl.core.shared_data import SharedData
 
 __all__ = [
     "SerialClientTrainer",
@@ -11,5 +11,4 @@ __all__ = [
     "PartitionedDataset",
     "ServerHandler",
     "SharedData",
-    "ShareMethod",
 ]
--- src/blazefl/core/shared_data.py
+++ src/blazefl/core/shared_data.py
@@ -1,74 +1,41 @@
-from enum import Enum
 from pathlib import Path
-from typing import Any, TypeVar, Type
+from typing import Generic, TypeVar
+from pydantic import BaseModel
 import torch
 
 
-class ShareMethod(Enum):
-    SHARED_MEMORY = "shared_memory"
-    DISK = "disk"
+SharedMemoryDataType = TypeVar("SharedMemoryDataType", bound=BaseModel)
+DiskDataType = TypeVar("DiskDataType", bound=BaseModel)
 
 
-T = TypeVar("T", bound="SharedData")
-
-
-class SharedData(object):
-    def __init__(self, disk_path: Path | None = None) -> None:
-        self.shared_memory_data: dict[str, Any] = {}
-        self.disk_data: dict[str, Any] = {}
+class SharedData(Generic[SharedMemoryDataType, DiskDataType]):
+    def __init__(
+        self,
+        shared_memory_data: SharedMemoryDataType,
+        disk_data: DiskDataType,
+        disk_path: Path,
+    ) -> None:
+        self.shared_memory_data = shared_memory_data
+        self.disk_data = disk_data
+        self.disk_data_type = type(self.disk_data)
         self.disk_path = disk_path
 
-    def set(
-        self, key: str, value: Any, method: ShareMethod = ShareMethod.SHARED_MEMORY
-    ) -> "SharedData":
-        match method:
-            case ShareMethod.SHARED_MEMORY:
-                self.shared_memory_data[key] = value
-            case ShareMethod.DISK:
-                self.disk_data[key] = value
-        return self
-
-    @classmethod
-    def copy(cls: Type[T], instance: T) -> T:
-        copied_instance = cls(instance.disk_path)
-        copied_instance.shared_memory_data = instance.shared_memory_data.copy()
-        copied_instance.disk_data = instance.disk_data.copy()
-        return copied_instance
-
-    def share(self) -> None:
-        for v in self.shared_memory_data.values():
-            if isinstance(v, torch.Tensor):
-                v.cpu().share_memory_()
-        if len(self.disk_data) > 0:
-            assert self.disk_path is not None, "Disk path is not provided."
+    def share(self) -> "SharedData[SharedMemoryDataType, DiskDataType]":
+        for field_key in self.shared_memory_data.model_fields.keys():
+            field_value = getattr(self.shared_memory_data, field_key)
+            if isinstance(field_value, torch.Tensor):
+                field_value.share_memory_()
+        if self.disk_data:
             self.disk_path.parent.mkdir(parents=True, exist_ok=True)
             torch.save(self.disk_data, self.disk_path)
-            self.disk_data = {}
+            del self.disk_data
+        return self
 
-    def get(self, key: str, method: ShareMethod = ShareMethod.SHARED_MEMORY) -> Any:
-        match method:
-            case ShareMethod.SHARED_MEMORY:
-                return self.shared_memory_data[key]
-            case ShareMethod.DISK:
-                assert self.disk_path is not None and self.disk_path.exists()
-                return torch.load(self.disk_path, weights_only=False)[key]
+    def get_shared_memory_data(self) -> SharedMemoryDataType:
+        return self.shared_memory_data
 
-    def get_all(self, method: ShareMethod | None = None) -> dict[str, Any]:
-        match method:
-            case ShareMethod.SHARED_MEMORY:
-                return self.shared_memory_data
-            case ShareMethod.DISK:
-                assert self.disk_path is not None and self.disk_path.exists()
-                return torch.load(self.disk_path, weights_only=False)
-            case None:
-                data = {}
-                data.update(self.shared_memory_data)
-                if self.disk_path is not None and self.disk_path.exists():
-                    data.update(torch.load(self.disk_path, weights_only=False))
-                return data
-
-    def clean(self) -> None:
-        self.shared_memory_data = {}
-        self.disk_data = {}
-        if self.disk_path is not None and self.disk_path.exists():
-            self.disk_path.unlink()
+    def get_disk_data(self) -> DiskDataType:
+        assert self.disk_path.exists()
+        loaded_data = torch.load(self.disk_path, weights_only=False)
+        assert isinstance(loaded_data, BaseModel)
+        return self.disk_data_type(**loaded_data.model_dump())
