eed05171dae2e8eac9b61500495d21f4c919d041
kitsuya0828 <kitsuyaazuma@gmail.com>
2024-12-09 17:16:32 +0900

    fix: remove preprocess from abstractmethod

--- src/blazefl/core/partitioned_dataset.py
+++ src/blazefl/core/partitioned_dataset.py
@@ -5,9 +5,6 @@ from torch.utils.data import DataLoader, Dataset
 
 class PartitionedDataset(ABC):
     @abstractmethod
-    def preprocess(self) -> None: ...
-
-    @abstractmethod
     def get_dataset(self, type_: str, cid: int | None) -> Dataset: ...
 
     @abstractmethod
