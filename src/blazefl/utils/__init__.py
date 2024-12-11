e563b76936ee19f131206e4a64c5716582582a96
kitsuya0828 <kitsuyaazuma@gmail.com>
2024-12-11 09:07:47 +0900

    feat: add FilteredDataset to utils

--- src/blazefl/utils/__init__.py
+++ src/blazefl/utils/__init__.py
@@ -1,3 +1,4 @@
+from blazefl.utils.dataset import FilteredDataset
 from blazefl.utils.serialize import deserialize_model, serialize_model
 
-__all__ = ["serialize_model", "deserialize_model"]
+__all__ = ["serialize_model", "deserialize_model", "FilteredDataset"]
