6d15cf244fa0875d1dc8d595b407af24508c5268
kitsuya0828 <kitsuyaazuma@gmail.com>
2024-12-13 02:07:49 +0900

    feat: evaluate clients (serial)

--- src/blazefl/contrib/fedavg.py
+++ src/blazefl/contrib/fedavg.py
@@ -136,6 +136,10 @@ class FedAvgSerialClientTrainer(
                 type_="train", cid=cid, batch_size=self.batch_size
             )
             pack = self.train(model_parameters, data_loader)
+            val_loader = self.dataset.get_dataloader(
+                type_="val", cid=cid, batch_size=self.batch_size
+            )
+            loss, acc = self.evaluate(val_loader)
             self.cache.append(pack)
 
     def train(
@@ -161,7 +165,34 @@ class FedAvgSerialClientTrainer(
 
         model_parameters = serialize_model(self.model)
 
-        return FedAvgUplinkPackage(model_parameters, data_size, {})
+        return FedAvgUplinkPackage(model_parameters, data_size)
+
+    def evaluate(self, test_loader: DataLoader) -> tuple[float, float]:
+        self.model.eval()
+
+        total_loss = 0.0
+        total_correct = 0
+        total_samples = 0
+        with torch.no_grad():
+            for inputs, labels in test_loader:
+                inputs = inputs.to(self.device)
+                labels = labels.to(self.device)
+
+                outputs = self.model(inputs)
+                loss = self.criterion(outputs, labels)
+
+                _, predicted = torch.max(outputs, 1)
+                correct = torch.sum(predicted.eq(labels)).item()
+
+                batch_size = labels.size(0)
+                total_loss += loss.item() * batch_size
+                total_correct += int(correct)
+                total_samples += batch_size
+
+        avg_loss = total_loss / total_samples
+        avg_acc = total_correct / total_samples
+
+        return avg_loss, avg_acc
 
     def uplink_package(self) -> list[FedAvgUplinkPackage]:
         package = deepcopy(self.cache)
