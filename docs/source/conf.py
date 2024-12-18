08810b26a6beb7eb3802eec5e437f0780e59ed14
kitsuya0828 <kitsuyaazuma@gmail.com>
2024-12-18 15:23:11 +0900

    docs: use book theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,23 +6,23 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'BlazeFL'
-copyright = '2024, kitsuya0828'
-author = 'kitsuya0828'
-release = '0.1.0rc2'
+project = "BlazeFL"
+copyright = "2024, kitsuya0828"
+author = "kitsuya0828"
+release = "0.1.0rc2"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "sphinx_book_theme"
+html_logo = "_static/logo.png"
+html_static_path = ["_static"]
