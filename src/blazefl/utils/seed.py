a70d6d692a7a059f27316095134b4a390b3c4ac6
kitsuyaazuma <kitsuyaazuma@gmail.com>
2025-06-03 23:57:39 +0900

    chore: improve type annotations
    Signed-off-by: kitsuyaazuma <kitsuyaazuma@gmail.com>

--- src/blazefl/utils/seed.py
+++ src/blazefl/utils/seed.py
@@ -1,8 +1,10 @@
 import os
 import random
 from dataclasses import dataclass
+from typing import Any
 
 import numpy as np
+import numpy.typing as npt
 import torch
 
 
@@ -55,20 +57,20 @@ class RandomState:
     A dataclass representing the random state for Python, NumPy, and PyTorch.
 
     Attributes:
-        random (tuple): The state of Python's random module.
-        environ (str): The PYTHONHASHSEED environment variable.
-        numpy (dict): The state of NumPy's RNG.
-        torch_seed (int): The initial seed for PyTorch.
-        torch_rng_state (torch.Tensor): The RNG state for PyTorch.
-        cuda (CUDARandomState | None): The CUDA-specific random state, if available.
+        _random (tuple): The state of Python's random module.
+        _environ (str): The PYTHONHASHSEED environment variable.
+        _numpy (tuple): The state of NumPy's RNG.
+        _torch_seed (int): The initial seed for PyTorch.
+        _torch_rng_state (torch.Tensor): The RNG state for PyTorch.
+        _cuda (CUDARandomState | None): The CUDA-specific random state, if available.
     """
 
-    random: tuple
-    environ: str
-    numpy: dict
-    torch_seed: int
-    torch_rng_state: torch.Tensor
-    cuda: CUDARandomState | None
+    _random: tuple[Any, ...]
+    _environ: str
+    _numpy: tuple[str, npt.NDArray[np.uint32], int, int, float]
+    _torch_seed: int
+    _torch_rng_state: torch.Tensor
+    _cuda: CUDARandomState | None
 
     @classmethod
     def get_random_state(cls, device: str) -> "RandomState":
@@ -81,28 +83,25 @@ class RandomState:
         Returns:
             RandomState: The captured random state.
         """
-        if device.startswith("cuda"):
-            return cls(
-                random.getstate(),
-                os.environ["PYTHONHASHSEED"],
-                np.random.get_state(),
-                torch.initial_seed(),
-                torch.cuda.get_rng_state(),
-                CUDARandomState(
-                    torch.cuda.initial_seed(),
-                    torch.backends.cudnn.deterministic,
-                    torch.backends.cudnn.benchmark,
-                    torch.cuda.get_rng_state(),
-                ),
-            )
-        return cls(
-            random.getstate(),
-            os.environ["PYTHONHASHSEED"],
-            np.random.get_state(),
-            torch.initial_seed(),
-            torch.get_rng_state(),
-            None,
+        _random = random.getstate()
+        _environ = os.environ["PYTHONHASHSEED"]
+        _numpy = np.random.get_state(legacy=True)
+        assert isinstance(_numpy, tuple)
+        _torch_seed = torch.initial_seed()
+        _torch_rng_state = torch.get_rng_state()
+
+        random_state = cls(
+            _random, _environ, _numpy, _torch_seed, _torch_rng_state, None
         )
+        if device.startswith("cuda"):
+            random_state._torch_rng_state = torch.cuda.get_rng_state()
+            random_state._cuda = CUDARandomState(
+                torch.cuda.initial_seed(),
+                torch.backends.cudnn.deterministic,
+                torch.backends.cudnn.benchmark,
+                torch.cuda.get_rng_state(),
+            )
+        return random_state
 
     @staticmethod
     def set_random_state(random_state: "RandomState") -> None:
@@ -115,14 +114,14 @@ class RandomState:
         Returns:
             None
         """
-        random.setstate(random_state.random)
-        os.environ["PYTHONHASHSEED"] = random_state.environ
-        np.random.set_state(random_state.numpy)
-        torch.manual_seed(random_state.torch_seed)
-        if random_state.cuda is not None:
-            torch.cuda.manual_seed(random_state.cuda.manual_seed)
-            torch.backends.cudnn.deterministic = random_state.cuda.cudnn_deterministic
-            torch.backends.cudnn.benchmark = random_state.cuda.cudnn_benchmark
-            torch.cuda.set_rng_state(random_state.cuda.cuda_rng_state)
+        random.setstate(random_state._random)
+        os.environ["PYTHONHASHSEED"] = random_state._environ
+        np.random.set_state(random_state._numpy)
+        torch.manual_seed(random_state._torch_seed)
+        if random_state._cuda is not None:
+            torch.cuda.manual_seed(random_state._cuda.manual_seed)
+            torch.backends.cudnn.deterministic = random_state._cuda.cudnn_deterministic
+            torch.backends.cudnn.benchmark = random_state._cuda.cudnn_benchmark
+            torch.cuda.set_rng_state(random_state._cuda.cuda_rng_state)
         else:
-            torch.set_rng_state(random_state.torch_rng_state)
+            torch.set_rng_state(random_state._torch_rng_state)
